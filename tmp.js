2d174596fd589c42d07e24af83fc50a6e2f228e5
james sewell <jamesjosephsewell@gmail.com>
2017-02-07 09:35:31 -0600

    solved some stuff

--- tmp.js
+++ tmp.js
@@ -11,6 +11,7 @@ var assert = require('chai').assert
 // Write a function called addOne() that returns the input number, plus one. 
 
 var addOne = function(num) {
+
 	return num + 1
 }
 
@@ -19,7 +20,8 @@ var addOne = function(num) {
 // Fix the following code so doubleNum(2) wil equal 4. 
 
 var doubleNum = function(num) {
-	num * 2
+
+	return num * 2
 }
 
 /**
@@ -28,7 +30,28 @@ var doubleNum = function(num) {
  * Write a function called sum that takes two numbers as
  * inputs and computes the sum of those two numbers.
  */
+ var sum = function(num1,num2){
 
+ 	if (typeof num1 !== "number" || typeof num2 !== "number"){
+
+ 		return null
+ 	}
+
+ 	var isNeg1 = isNegative(num1)
+ 	var isNeg2 = isNegative(num2)
+
+ 	if (isNeg1 === true || isNeg2 === true){
+
+ 		return null
+ 	}
+
+ 	return num1 + num2
+ }
+
+ // PART 2.5
+
+// modify your sum() function so that it will return null
+// if either of the inputs is not a number.
 
 // PART 3
 
@@ -37,13 +60,30 @@ var doubleNum = function(num) {
 // repeating yourself, use your sum function inside 
 // of your average function.
 
+var average = function(num1,num2){
+
+	sumResult = sum(num1,num2)
+
+	return sumResult / 2
+}
+
 
 // Part 4
 
 // Write a function called isNegative that will tell 
 // whether a number is negative or not.
 
+var isNegative = function(num){
 
+	if (num < 0){
+
+		return true
+
+	}
+
+	return false
+
+}
 // Now, modify your sum function again, so that it 
 // will return null if any of the inputs is negative.
 // To avoid repeating yourself, use your isNegative 
@@ -56,12 +96,28 @@ var doubleNum = function(num) {
 // input numbers. You can do it using nested if statements,
 // boolean operators, or both (but not neither).
 
+var minimum = function(num1,num2,num3,num4){
+	
+	return Math.min(num1,num2,num3,num4)
+
+}
 
 // Part 6
 
 // Using logical operators, write a function that will
 // return true if either input is a string, but not 
-// both or neither. 
+// both or neither.
+
+var justOneString = function(string1,string2){
+
+	if (typeof string1 !== 'string' && typeof string2 === 'string'){
+		return true 
+	}
+	if (typeof string1 === 'string' && typeof string2 !== 'string'){
+		return true 
+	}
+	return false;
+}
 
 
 // HARD MODE
@@ -75,16 +131,18 @@ var doubleNum = function(num) {
 // modify global variables, although that's not a good
 // pattern for production code.
 
-var doTwice = function() {
-
+var doTwice = function(something) {
+	something()
+	something()
 }
 
 var helloWorld = function() {
 	console.log('hello world')
 }
 
+doTwice(helloWorld) 
 
-doTwice(helloWorld) // should print 'hello world' to the console twice. 
+ //should print 'hello world' to the console twice. 
 
 
 // Part 8
@@ -94,8 +152,11 @@ doTwice(helloWorld) // should print 'hello world' to the console twice.
 // the value of a certain global variable, called ORACLE, is 
 // "YES." Otherwise, it will does nothing.
 
-var conditionallyInvoke = function() {
-
+var conditionallyInvoke = function(testing) {
+	if(ORACLE === 'YES'){
+		testing()
+		
+	}
 }
 
 var ORACLE = 'NO'
@@ -115,13 +176,15 @@ conditionallyInvoke(helloWorld) // should print hello world
 
 var factory = function() {
 
+
 }
 
 
 // Part 10
 
-var factory2 = function() {
+var factory2 = function(phrase) {
 	// you want more?
+	return phrase
 }
 	
 // Part 11
@@ -129,7 +192,7 @@ var factory2 = function() {
 var factory3 = function(stuff) {
 	// This can't be healthy...	
 }
-
+ 
 console.log('********************************************************************************************************************************************')
 console.log('')
 console.log('Test Results Below')
@@ -167,8 +230,18 @@ describe('Function', function() {
 			assert.equal(19, sum(8, 11))
 			assert.equal(104, sum(4, 100))
 			assert.equal(6176, sum(867, 5309))
+		})
+	})
+	describe('#sum()', function(){
+		it('should return null if either input is not a number', function(){
+			checkFuncBasics('sum',2)
 			assert.equal(null, sum('867', 5309))
 			assert.equal(null, sum(true, 5))
+		})
+	})
+	describe('#sum()', function(){
+		it('should return null if either input is negative', function(){
+			checkFuncBasics('sum',2)
 			assert.equal(null, sum(5,-5))
 		})
 	})
@@ -197,7 +270,7 @@ describe('Function', function() {
 		})
 	})
 	describe('#justOneString', function(){
-		it('should return true if one input is a string', function(){
+		it('should return true if exactly one input is a string', function(){
 			checkFuncBasics('justOneString',2)
 			assert.equal(true, justOneString('a',5))
 			assert.equal(true, justOneString(6,'dotron'))
--- worksheet.js
+++ worksheet.js
@@ -10,6 +10,7 @@
 // Write a function called addOne() that returns the input number, plus one. 
 
 var addOne = function(num) {
+
 	return num + 1
 }
 
@@ -18,7 +19,8 @@ var addOne = function(num) {
 // Fix the following code so doubleNum(2) wil equal 4. 
 
 var doubleNum = function(num) {
-	num * 2
+
+	return num * 2
 }
 
 /**
@@ -27,6 +29,23 @@ var doubleNum = function(num) {
  * Write a function called sum that takes two numbers as
  * inputs and computes the sum of those two numbers.
  */
+ var sum = function(num1,num2){
+
+ 	if (typeof num1 !== "number" || typeof num2 !== "number"){
+
+ 		return null
+ 	}
+
+ 	var isNeg1 = isNegative(num1)
+ 	var isNeg2 = isNegative(num2)
+
+ 	if (isNeg1 === true || isNeg2 === true){
+
+ 		return null
+ 	}
+
+ 	return num1 + num2
+ }
 
  // PART 2.5
 
@@ -40,13 +59,30 @@ var doubleNum = function(num) {
 // repeating yourself, use your sum function inside 
 // of your average function.
 
+var average = function(num1,num2){
+
+	sumResult = sum(num1,num2)
+
+	return sumResult / 2
+}
+
 
 // Part 4
 
 // Write a function called isNegative that will tell 
 // whether a number is negative or not.
 
+var isNegative = function(num){
 
+	if (num < 0){
+
+		return true
+
+	}
+
+	return false
+
+}
 // Now, modify your sum function again, so that it 
 // will return null if any of the inputs is negative.
 // To avoid repeating yourself, use your isNegative 
@@ -59,12 +95,28 @@ var doubleNum = function(num) {
 // input numbers. You can do it using nested if statements,
 // boolean operators, or both (but not neither).
 
+var minimum = function(num1,num2,num3,num4){
+	
+	return Math.min(num1,num2,num3,num4)
+
+}
 
 // Part 6
 
 // Using logical operators, write a function that will
 // return true if either input is a string, but not 
-// both or neither. 
+// both or neither.
+
+var justOneString = function(string1,string2){
+
+	if (typeof string1 !== 'string' && typeof string2 === 'string'){
+		return true 
+	}
+	if (typeof string1 === 'string' && typeof string2 !== 'string'){
+		return true 
+	}
+	return false;
+}
 
 
 // HARD MODE
@@ -78,16 +130,18 @@ var doubleNum = function(num) {
 // modify global variables, although that's not a good
 // pattern for production code.
 
-var doTwice = function() {
-
+var doTwice = function(something) {
+	something()
+	something()
 }
 
 var helloWorld = function() {
 	console.log('hello world')
 }
 
+doTwice(helloWorld) 
 
-doTwice(helloWorld) // should print 'hello world' to the console twice. 
+ //should print 'hello world' to the console twice. 
 
 
 // Part 8
@@ -97,8 +151,11 @@ doTwice(helloWorld) // should print 'hello world' to the console twice.
 // the value of a certain global variable, called ORACLE, is 
 // "YES." Otherwise, it will does nothing.
 
-var conditionallyInvoke = function() {
-
+var conditionallyInvoke = function(testing) {
+	if(ORACLE === 'YES'){
+		testing()
+		
+	}
 }
 
 var ORACLE = 'NO'
@@ -118,13 +175,15 @@ conditionallyInvoke(helloWorld) // should print hello world
 
 var factory = function() {
 
+
 }
 
 
 // Part 10
 
-var factory2 = function() {
+var factory2 = function(phrase) {
 	// you want more?
+	return phrase
 }
 	
 // Part 11
@@ -132,4 +191,4 @@ var factory2 = function() {
 var factory3 = function(stuff) {
 	// This can't be healthy...	
 }
-
+ 
